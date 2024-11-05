f65cc3582e409e34ee00fa735354d9f3cb2216c9
dean <deanvelopermn@gmail.com>
2024-11-04 17:37:40 -0800

    add numbers to channel types

--- src/model/Channel.zig
+++ src/model/Channel.zig
@@ -45,20 +45,20 @@ newly_created: Omittable(bool) = .omit,
 
 pub const jsonStringify = jconfig.stringifyWithOmit;
 
-pub const Type = enum {
-    guild_text,
-    dm,
-    guild_voice,
-    group_dm,
-    guild_category,
-    guild_announcement,
-    announcement_thread,
-    public_thread,
-    private_thread,
-    guild_stage_voice,
-    guild_directory,
-    guild_forum,
-    guild_media,
+pub const Type = enum(u8) {
+    guild_text = 0,
+    dm = 1,
+    guild_voice = 2,
+    group_dm = 3,
+    guild_category = 4,
+    guild_announcement = 5,
+    announcement_thread = 10,
+    public_thread = 11,
+    private_thread = 12,
+    guild_stage_voice = 13,
+    guild_directory = 14,
+    guild_forum = 15,
+    guild_media = 16,
 
     pub const jsonStringify = jconfig.stringifyEnumAsInt;
 };
